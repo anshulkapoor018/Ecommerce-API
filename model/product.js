17ed690dc21a19ddd278156583e9fd64c0195f33
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-08 20:55:39 +0530

    Final Commit

--- model/product.js
+++ model/product.js
@@ -4,10 +4,7 @@ var Schema = mongoose.Schema;
 var product = new Schema({
     title: String,
     price: Number,
-    likes: {
-        type: Number,
-        default: 0
-    }
+    likes: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Product', product);
